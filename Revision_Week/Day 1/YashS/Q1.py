aed5a03c7027a52ffe0da3824b79a91554df8196
Yash Salvi <yashsalvi075@gmail.com>
2026-09-14 15:46:34 +0530

    Create Q1.py

---
+++ Revision_Week/Day 1/YashS/Q1.py
@@ -0,0 +1,7 @@
+name = input("Enter user_name: ")
+age = int(input("Enter user_age: "))
+city = input("Enter user_city; ")
+
+print(f"User_Name: {name}")
+print(f"User_Age: {age}")
+print(f"User_City: {city}")
