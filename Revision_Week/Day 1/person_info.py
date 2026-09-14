0a043ed58d8fab69aafeebcfc6343613f4c38d06
Krishna-0911 <24030332905053@dbatu.ac.in>
2026-09-15 00:59:39 +0530

    Create person_info.py

---
+++ Revision_Week/Day 1/person_info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+city = str(input("Enter your city:"))
+
+print(f"Your name is {name}")
+print(f"Your age {age}")
+print(f"and the city is {city}")
