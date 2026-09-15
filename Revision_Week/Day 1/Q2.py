fc07c9f4efe71f57a3d39dea98d4232532e072b6
Loukik Vishe <loukik.vishe@gmail.com>
2026-09-15 18:59:06 +0530

    Add files via upload

---
+++ Revision_Week/Day 1/Q2.py
@@ -0,0 +1,8 @@
+a = int(input("Enter Number 1:"))
+b = int(input("Enter Number 2:"))
+
+print("Addition a+b:", a+b)
+print("Subtraction a-b:", a-b)
+print("Multiplication a*b:", a*b)
+print("Division a/b:", a/b)
+print("Remainder a%b:", a%b)
---
+++ Revision_Week/Day 1/Q4.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number :"))
+
+if a > 0:
+    print("The NUmber is POSITIVE")
+elif a < 0:
+    print("The Number is NEGATIVE")
+else:
+    print("The Number is ZERO")
