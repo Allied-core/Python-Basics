a5a5183e62466fac44fe9c4d9dcbc26eec9bff8d
DeepTure97 <deepd.ture123@gmail.com>
2026-09-16 12:44:34 +0530

    Add files via upload
    Day 1

---
+++ Revision_Week/Day 1/positivenegative.py
@@ -0,0 +1,8 @@
+Number= int(input("Enter a number: "))
+if Number > 0:
+   print("number is positive :")
+elif Number <  0:
+   print("number is negative :") 
+else:
+   print("Number is zero:")   
+
