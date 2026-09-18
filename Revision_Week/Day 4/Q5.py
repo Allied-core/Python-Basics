c28095eff01a277a8009fbe86b2bb62c4c8083e8
Yash Salvi <yashsalvi075@gmail.com>
2026-09-18 14:55:41 +0530

    Create Q5.py

---
+++ Revision_Week/Day 4/Q5.py
@@ -0,0 +1,20 @@
+try :
+   marks = int(input("Enter the Marks: "))
+except:
+   print("Invalid Input")
+
+def calculate_grade(marks):
+    if marks >= 90 and marks <= 100 :
+       print("Grade:A")
+    elif marks >= 80 and marks < 90 :
+       print("Grade:B")
+    elif marks >= 70 and marks < 80 :
+       print("Grade:C")
+    elif marks >= 60 and marks < 70 :
+       print("Grade:D")
+    elif marks < 60 :
+       print("Grade:F")
+    else:
+       print("Invalid Marks")
+
+calculate_grade(marks)
