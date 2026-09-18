ad15faddb0e177f4f855d0e5d509a65c04dbb454
Yash Salvi <yashsalvi075@gmail.com>
2026-09-18 14:50:51 +0530

    Create Q1.py

---
+++ Revision_Week/Day 4/Q1.py
@@ -0,0 +1,11 @@
+Student = {
+     "Name":"Student_1",
+     "Age":20,
+     "Course":"CSE",
+     "Marks":80
+}
+
+print(f"Student_Name : {Student['Name']}")
+print(f"Student_Age : {Student['Age']}")
+print(f"Student_Course : {Student['Course']}")
+print(f"Student_Marks : {Student['Marks']}")
