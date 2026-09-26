85bfc61bb56a49ffbf48fc141b2ca6051ac11933
Ritesh Birhade <riteshbirhade2005@gmail.com>
2026-09-26 20:44:22 +0530

    Add files via upload

---
+++ Revision_Week/Day 1/Q1_RVB.py
@@ -0,0 +1,9 @@
+# Personal Introduction
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print("\nMy name is", name)
+print("I am", age, "years old")
+print("I live in", city)
---
+++ Revision_Week/Day 1/Q3_RVB.py
@@ -0,0 +1,8 @@
+# Even or Odd
+
+num = int(input("Enter an integer: "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
